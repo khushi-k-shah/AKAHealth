ed1681deea4e0172e9b489796c5cab65828bdf24
Aarushi Upadhyaya <aarushi.e.upadhyaya@gmail.com>
2021-03-26 20:56:34 -0400

    Update portal.sql

--- portal.sql
+++ portal.sql
@@ -19,7 +19,8 @@ CREATE TABLE IF NOT EXISTS `Basic_Patient_Info` (
   `phone_number` int(10) NOT NULL,
   `address` varchar(50) NOT NULL,
   `current_medication` varchar(50) NOT NULL,
-  `underlying_health_condition` varchar(50) NOT NULL
+  `underlying_health_condition` varchar(50) NOT NULL,
+  `insurance_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `Basic_Patient_Info`
@@ -49,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `Appointments_Table` (
   `patient_ID` int(11) NOT NULL,
   `symptoms` varchar(50) NOT NULL,
   `treatment` varchar(50) NOT NULL,
-  `DATETIME appointment_time` int(11) NOT NULL,
+  `appointment_time` DATETIME(11) NOT NULL,
   `doctor_type` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -99,10 +100,30 @@ ALTER TABLE `Employee_Accesses`
 
 
 INSERT INTO `Basic_Patient_Info` (`patient_ID`, `name`, `age`, `gender`, `phone_number`, `address`, `current_medication`, `underlying_health_condition`) VALUES
-(1250, 'Bob', 21, 'male', 4087449840, '125 Sesame Street', 'percocet', 'Moderate pain'),
-(1251, 'Hailey', 12, 'female', 6507843840, '332 State Street', 'tylenol', 'none'),
-(1252, 'Sara', 57, 'female', 4056349840, '370 South Street', 'vitamin', 'Iron deficiency'),
-(1253, 'Anisha', 42, 'female', 4087673840, '154 Season Street', 'N/A', 'N/A'),
-(1254, 'Naya', 33, 'female', 4784849840, '384 Saratoga Street', 'N/A', 'N/A'),
-(1255, 'Gabe', 25, 'male', 4087840230, '496 Seeme Street', 'N/A', 'N/A'),
-(1256, 'Timothy', 17, 'male', 4087573840, '204 Sequioa Street', 'N/A', 'N/A');
+(1250, 'Bob', 21, 'male', 4087449840, '125 Sesame Street', 'percocet', 'Moderate pain', 1),
+(1251, 'Hailey', 12, 'female', 6507843840, '332 State Street', 'tylenol', 'none', 2),
+(1252, 'Sara', 57, 'female', 4056349840, '370 South Street', 'vitamin', 'Iron deficiency', 3),
+(1253, 'Anisha', 42, 'female', 4087673840, '154 Season Street', 'N/A', 'N/A', 2),
+(1254, 'Naya', 33, 'female', 4784849840, '384 Saratoga Street', 'N/A', 'N/A', 1),
+(1255, 'Gabe', 25, 'male', 4087840230, '496 Seeme Street', 'N/A', 'N/A', 1),
+(1256, 'Timothy', 17, 'male', 4087573840, '204 Sequioa Street', 'N/A', 'N/A', 3);
+
+
+INSERT INTO `Employee_Info` (`employee_ID`, `name`, `employee_type`, `office_name`) VALUES
+(1000, 'Aarushi', 21, 'doctor', 'A'),
+(1001, 'Khushi', 12, 'receptionist', 'A'),
+(1002, 'Miranda', 57, 'doctor', 'B'),
+(1003, 'Natasha', 42, 'receptionist', 'B');
+
+INSERT INTO `Appointments_Table` (`appointment_ID`, `employee_ID`, `patient_ID`, `symptoms`, `treatment`, `appointment_time`, `doctor_type`) VALUES
+(1, 1000, 1256, 'cough', 'motrin prescribed', 2020-11-21 11:23:43),
+(2, 1002, 1254, 'runny nose', 'nasal spray prescribed', 2021-1-19 10:02:56),
+(3, 1000, 1253, 'stomache ache', 'N/A', 2021-11-16 13:07:22),
+(4, 1002, 1251, 'rash', 'ointment prescribed', 2020-11-03 18:27:24);
+
+INSERT INTO `Insurance` (`insurance_ID`, `type`, `company_name`) VALUES
+(1, 'health', 'aetna'),
+(2, 'dental', 'blue'),
+(3, 'health', 'green');
+
+
