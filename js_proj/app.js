c1a9318d790645d847c3d183a09614fdd88699da
Khushi Shah <khushishha@outlook.com>
2021-05-03 19:12:52 -0700

    forms readonly attributes added

--- js_proj/app.js
+++ js_proj/app.js
@@ -16,8 +16,8 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "password",
-    database: "cs348proj",
+    password: "",
+    database: "portal",
     multipleStatements: true
   });
 
